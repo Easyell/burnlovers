f4d15f8ff981e80e946d0cfcfd78def2be9747d9
guoshencheng <648772021@qq.com>
2015-11-07 13:17:13 +0800

    减速火焰速度

--- public/js/main.js
+++ public/js/main.js
@@ -11,5 +11,15 @@ document.body.appendChild(renderer.view);
 var stage = new PIXI.Container();
 fire.createSelf(function(fire) {
     fire.play();
+    fire.scale.x = 0.3
+    fire.scale.y = 0.3
     stage.addChild(fire)
+    animate();
 });
+
+function animate() {
+    // render the stage container
+    renderer.render(stage);
+
+    requestAnimationFrame(animate);
+}
