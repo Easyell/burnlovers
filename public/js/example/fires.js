87accfa1e40d9c20104d3b264b1f355c092618f1
guoshencheng <648772021@qq.com>
2015-11-07 11:45:32 +0800

    add fire.js

---
+++ public/js/example/fires.js
@@ -0,0 +1,25 @@
+/**
+ * Created by guoshencheng on 15/11/7.
+ */
+
+var PIXI = require('../bower_components/pixi.js/bin/pixi.js')
+
+var Fire = new Object()
+
+Fire.createSelf = function(cb) {
+    PIXI.loader.add('/assets/fires/fires.json').load(onAssertsLoad(cb))
+}
+
+function onAssertsLoad(cb) {
+    var frames = []
+    for (var i = 0; i < 4; i ++) {
+        frames.push(PIXI.Texture.fromFrame('fire' + (i + 1) + '.png'))
+    }
+    Fire = new PIXI.extras.MovieClip(frames)
+    Fire.position.set(300)
+    Fire.anchor.set(0.5);
+    Fire.animationSpeed = 0.5;
+    cb()
+}
+
+module.exports = Fire
--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,11 @@
  */
 var hello = require('./example/hello');
 var PIXI = require('./bower_components/pixi.js/bin/pixi.js')
+var fire = require('./example/fires')
 var renderer = new PIXI.WebGLRenderer(800, 600)
 document.body.appendChild(renderer.view)
 var stage = new PIXI.Container()
+fire.createSelf(function() {
+    fire.play()
+    stage.addChild(fire)
+})
