868b0c29eed39cecdb9f2d28ec2669b402803285
guoshencheng <648772021@qq.com>
2015-11-07 17:08:01 +0800

    add speed and direction and move

--- public/js/sprites/lovers.js
+++ public/js/sprites/lovers.js
@@ -1,9 +1,16 @@
 var sprite = require('./sprite');
-var R = require('../resource');
+var R = require('../resource')
 var lovers = sprite.getIm({
     img: R.lovers,
     'position.set': [280,10],
     'scale.x': 1,
     'scale.y': 1
 })
+lovers.direction = [0, 0]
+lovers.speed = 0
+lovers.move = function(dt) {
+    lovers.x = lovers.direction[0] * lovers.speed * dt
+    lovers.y = lovers.direction[1] * lovers.speed * dt
+}
+
 module.exports = lovers;
