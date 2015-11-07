1e117789545f9206b4a3a43085d449dd0bf78e58
zhou-yg <zhou.y.g890@gmail.com>
2015-11-07 12:21:00 +0800

    添加gulp，简化启动

---
+++ tasks/webpack.js
@@ -0,0 +1,31 @@
+/**
+ * Created by zyg on 15/8/22.
+ */
+var path = require('path');
+var gutil = require('gulp-util');
+
+var exec = require('child_process').exec;
+
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+
+var webpackConfig = require('../webpack.config.js');
+
+module.exports = function (gulp) {
+
+  gulp.task('webpack', function (callback) {
+    webpack(webpackConfig, function (err, stats) {
+      if (err) throw new gutil.PluginError('webpack', err);
+      gutil.log("[webpack]", stats.toString({
+          // output options
+        })
+      );
+    });
+  });
+  gulp.task('webpackDevServer', function(callback) {
+    // Start a webpack-dev-server
+    exec('webpack-dev-server --hot --inline --display-error-details --port '+webpackConfig.webpackDevPort,function(){
+      console.log.apply(console,arguments);
+    });
+  });
+};
