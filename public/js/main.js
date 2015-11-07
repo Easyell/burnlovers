44145fa97737b03cb916dda388dd4d29b26a13e4
guoshencheng <648772021@qq.com>
2015-11-07 10:34:36 +0800

    尝试加入pix.js，使用node_module导入没有Container()方法好奇怪，就用了bower，但是警告说最好使用源文件

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,7 @@
  * Created by zyg on 15/11/6.
  */
 var hello = require('./example/hello');
+var PIXI = require('./bower_components/pixi.js/bin/pixi.js')
+var renderer = new PIXI.WebGLRenderer(800, 600)
+document.body.appendChild(renderer.view)
+var stage = new PIXI.Container()
