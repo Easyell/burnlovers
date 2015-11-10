0c755a528f53b022172942da7b8021a3a157aef6
zhouyg <yunge@taovip.com>
2015-11-10 10:12:17 +0800

    加入图片压缩

---
+++ tasks/tinyPng.js
@@ -0,0 +1,17 @@
+/**
+ * Created by zyg on 15/11/10.
+ */
+var tinyPng = require('gul-tinypng');
+var path = require('path');
+var apiKey = '_U9AFU6XG2QpeMT3__2pWl5pOW10NvfL';
+var pngDir = path.resolve(__dirname,'../public/assets/**/*.png');
+var dest = path.resolve(__dirname,'../public/compress/');
+
+module.exports = function(gulp){
+
+  gulp.task('tinyPng',function(){
+    gulp.src(pngDir)
+      .pipe(tinyPng(apiKey))
+      .pipe(gulp.dest(dest));
+  });
+};
